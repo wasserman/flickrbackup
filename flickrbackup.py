ac18aacd8be7c85df56176a960fe867bc2bc8d87
Tiago Bortoletto Vaz <tiago@debian.org>
2011-09-08 03:22:29 -0300

    Cleaning white spaces.

--- flickrbackup.py
+++ flickrbackup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 #
-# flickrbackup - a simple python script to grab all your photos from flickr, 
-#                dump into a directory - organised into folders by set - 
+# flickrbackup - a simple python script to grab all your photos from flickr,
+#                dump into a directory - organised into folders by set -
 #                along with any favourites you have saved.
 #
 # Version: 1.2
 #
 # Original Author: colm - AT - allcosts.net  - Colm MacCarthaigh - 2008-01-21
 #
-# Modified by: Dan Benjamin - http://hivelogic.com							
+# Modified by: Dan Benjamin - http://hivelogic.com
 #
-# Forked, renamed and refactored by Tiago Bortoletto Vaz <tiago@debian.org> 
+# Forked, renamed and refactored by Tiago Bortoletto Vaz <tiago@debian.org>
 # in Thu Sep  8 00:04:58 UTC 2011
 #
 # License: Apache 2.0 - http://www.apache.org/licenses/LICENSE-2.0.html
